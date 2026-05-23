b4f13dc3f318b0f11551e172cab709b74ef385e6
binnie-ch <binnie.bank@gmail.com>
2026-05-23 10:09:55 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,8 +9,8 @@ import pytz
 # TELEGRAM CONFIG
 # =========================
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+BOT_TOKEN = "8568497873:AAHEXglTw7nowIhX27AmPnKCs24ku6lF6gc"
+CHAT_ID = "8540013665"
 
 # =========================
 # SEND TELEGRAM ALERT
